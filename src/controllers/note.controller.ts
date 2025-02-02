256b129aa7a958c7eefa2b112eba8262a3331d30
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-02 19:00:51 +0530

    [Vineet] Operation: ADD :: Add note.controller.ts Contains create, getById and deleteById methods

---
+++ src/controllers/note.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import {createNote, getNoteById, deleteNoteById,} from '../services/note.services';
+
+export default class NoteController {
+    public create = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const note = await createNote(req.body);
+            res.status(201).json({ message: 'Note created successfully', note });
+        } catch (error: any) {
+            res.status(500).json({ message: error.message });
+        }
+    };
+
+    public getById = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const note = await getNoteById(req.params.id);
+            if (!note) {
+                res.status(404).json({ message: 'Note not found' });
+                return;
+            }
+            res.status(200).json({ note });
+        } catch (error: any) {
+            res.status(500).json({ message: error.message });
+        }
+    };
+
+    public deleteById = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const note = await deleteNoteById(req.params.id);
+            if (!note) {
+                res.status(404).json({ message: 'Note not found' });
+                return;
+            }
+            res.status(200).json({ message: 'Note deleted successfully' });
+        } catch (error: any) {
+            res.status(500).json({ message: error.message });
+        }
+    };
+
+}
