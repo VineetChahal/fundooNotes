fa1459f2c1c54d51b3660cf9b6bae50950dc5bae
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-02 19:02:22 +0530

    [Vineet] Operation: ADD :: Add note.services.ts

---
+++ src/services/note.services.ts
@@ -0,0 +1,17 @@
+// src/services/note.services.ts
+import { Note } from '../models/note.model';
+
+// create note
+export const createNote = async (noteData: any) => {
+    return await Note.create(noteData);
+};
+
+// get note
+export const getNoteById = async (noteId: string) => {
+    return await Note.findById(noteId);
+};
+
+// delete note
+export const deleteNoteById = async (noteId: string) => {
+    return await Note.findByIdAndDelete(noteId);
+};
