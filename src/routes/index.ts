7081279233900e5bbb00946de18261311f4c8b4e
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-02 18:49:04 +0530

    [Vineet] Operation: ADD :: Add note routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express, { IRouter } from 'express';
-import UserRoutes from './user.route';
+import UserRoutes from './user.route'; // import user routes
+import NoteRoutes from './note.route'; // Import note routes
 
 const router = express.Router();
 
@@ -7,7 +8,8 @@ const routes = (): IRouter => {
     router.get('/', (req, res) => {
         res.json('Welcome');
     });
-    router.use('/users', new UserRoutes().getRoutes());
+    router.use('/users', new UserRoutes().getRoutes()); // Add user routes
+    router.use('/notes', new NoteRoutes().getRoutes()); // Add note routes
 
     return router;
 };
