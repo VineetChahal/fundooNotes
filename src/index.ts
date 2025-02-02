7880ffd3c053f525fcec2bfe34ed05c8da5a52c2
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-02 18:28:35 +0530

    [Vineet] Operation: UPDATE :: Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/database';
-import userRoutes from './routes/user.route';
+import routes from './routes';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
-app.use('/api/users', userRoutes);
+app.use(express.json());
+app.use('/api', routes());
 
 connectDB();
 const PORT = process.env.PORT || 3000;
