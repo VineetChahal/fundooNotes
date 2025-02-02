133e56876ffc0766487dc707a236c1861a7b5fbf
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-02 18:53:53 +0530

    [Vineet] Operation: ADD :: Add note.route.ts Contains- post, ge, delete routes

---
+++ src/routes/note.route.ts
@@ -0,0 +1,29 @@
+import express, { IRouter } from 'express';
+import NoteController from '../controllers/note.controller';
+import { createNoteValidation, updateNoteValidation } from '../validators/note.validator';
+
+class NoteRoutes {
+    private NoteController = new NoteController();
+    private router = express.Router();
+
+    constructor() {
+        this.routes();
+    }
+
+    private routes = () => {
+        // Create a new note
+        this.router.post('/notes', createNoteValidation, this.NoteController.create);
+
+        // Get a note by ID
+        this.router.get('/notes/:id', this.NoteController.getById);
+
+        // Delete a note by ID
+        this.router.delete('/notes/:id', this.NoteController.deleteById);
+    };
+
+    public getRoutes = (): IRouter => {
+        return this.router;
+    };
+}
+
+export default NoteRoutes;
