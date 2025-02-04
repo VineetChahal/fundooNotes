5e6b72d799c6d371bc45e29c257470e048ed8e1f
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-04 11:13:29 +0530

    [Vineet] Operation: UPDATE :: Add userId to the note interface

--- src/interfaces/note.interface.ts
+++ src/interfaces/note.interface.ts
@@ -1,6 +1,11 @@
+import { ObjectId } from "mongoose";
+
 export interface INote extends Document {
     title: string;
     description: string;
     color: string;
-    userId: string; // To associate the note with a user
+    // userId: string;
+    userId: ObjectId; // To associate the note with a user
+
+
 }
