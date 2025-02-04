d257ec69acdecfd81b357b08f1d932fd895fc29e
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-04 11:07:43 +0530

    [Vineet] Operation: UPDATE :: Update note.validator.ts added a validation for updating a note 'updateNoteValidation'

--- src/validators/note.validator.ts
+++ src/validators/note.validator.ts
@@ -5,3 +5,9 @@ export const createNoteValidation = [
     body('description').isString().notEmpty().withMessage('Description is required'),
     body('color').optional().isString().withMessage('Color must be a string')
 ];
+
+export const updateNoteValidation = [
+    body('title').optional().isString().withMessage('Title must be a string'),
+    body('description').optional().isString().withMessage('Description must be a string'),
+    body('color').optional().isString().withMessage('Color must be a string')
+];
