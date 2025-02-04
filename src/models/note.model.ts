08cae2af1ee268fad150e00b5b4fcc5e6196c35c
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-04 11:09:57 +0530

    [Vineet] Operation: ADD :: Add userId to the note schema

--- src/models/note.model.ts
+++ src/models/note.model.ts
@@ -1,11 +1,12 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model, Document, Mongoose } from 'mongoose';
 import { INote } from '../interfaces/note.interface';
 
 const noteSchema = new Schema<INote>({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    color: { type: String, default: '#FFFFFF' }, // Default color is white
-    userId: { type: String, required: true } // Reference to the user who created the note
-}, { timestamps: true });
+    color: { type: String, default: '#FFFFFF' },
+    // userId: {type: String, required: true}, // user id to create a link between note and user
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true } // user id to create a link between note and user
+});
 
 export const Note = model<INote>('Note', noteSchema);
