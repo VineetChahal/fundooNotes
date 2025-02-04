5ffa281976791cacf0fdde371f6d898c0f388eec
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-04 11:03:20 +0530

    [Vineet] Operation: UPDATE :: Update index.ts change port and log message

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,4 @@ app.use('/api', routes());
 
 connectDB();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server is running at http://localhost:${PORT}/api`));
