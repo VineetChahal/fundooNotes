a18bf1790c36c9848cd91f2350e99a4807d210cf
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-05 15:50:05 +0530

    [Vineet] Operation: UPDATE :: Update auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,15 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
-import { verifyToken } from '../utils/jwt';
+// import { verifyToken } from '../utils/jwt';
+import jwt from 'jsonwebtoken';
 
 export const authenticate = (req: Request, res: Response, next: NextFunction): void => {
     const token = req.header('Authorization')?.split(' ')[1];
+    // console.log("token ---------------------------," + token);
+    
     if (!token) {
         res.status(401).json({ message: 'Access denied' });
         return;
     }
     try {
-        const decoded = verifyToken(token);
-        (req as any).user = decoded;
+
+        const tokenvalue = jwt.verify(token, process.env.JWT_SECRET as string) as {id:string};
+        // console.log(`decoded msg----------------------------------,`,tokenvalue );
+        req.body.userId = tokenvalue.id;
         next();
     } catch (error) {
         res.status(400).json({ message: 'Invalid token' });
