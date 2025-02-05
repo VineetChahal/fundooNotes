389ba7b95e0d652a2cfe2810afbd0ce5a256a1e9
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-05 15:48:14 +0530

    [Vineet] Operation: UPDATE :: Update note model

--- src/models/note.model.ts
+++ src/models/note.model.ts
@@ -1,12 +1,12 @@
-import { Schema, model, Document, Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 import { INote } from '../interfaces/note.interface';
 
-const noteSchema = new Schema<INote>({
+const noteSchema = new mongoose.Schema<INote>({
     title: { type: String, required: true },
     description: { type: String, required: true },
     color: { type: String, default: '#FFFFFF' },
     // userId: {type: String, required: true}, // user id to create a link between note and user
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true } // user id to create a link between note and user
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true } // user id to create a link between note and user
 });
 
-export const Note = model<INote>('Note', noteSchema);
+export const Note = mongoose.model<INote>('Note', noteSchema);
