ef781d1033ceb61755cea237e6046fdc69889859
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-05 15:51:21 +0530

    [Vineet] Operation: UPDATE :: Update jwt

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,3 +1,17 @@
+// import jwt from 'jsonwebtoken';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const JWT_SECRET = process.env.JWT_SECRET!;
+
+// export const generateToken = (payload: object) => {
+//     return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+// };
+
+// export const verifyToken = (token: string) => {
+//     return jwt.verify(token, JWT_SECRET);
+// };
+
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -6,8 +20,4 @@ const JWT_SECRET = process.env.JWT_SECRET!;
 
 export const generateToken = (payload: object) => {
     return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
-};
-
-export const verifyToken = (token: string) => {
-    return jwt.verify(token, JWT_SECRET);
 };
