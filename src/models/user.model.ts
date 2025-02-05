5291acae4e97a8a597c1791681cc8510bacf8685
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-05 15:54:09 +0530

    [Vineet] Operation: UPDATE :: Update user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,9 +2,12 @@ import mongoose from 'mongoose';
 import { IUser } from '../interfaces/user.interface';
 
 const userSchema = new mongoose.Schema<IUser>({
-    username: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: {type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    note:{type: mongoose.Schema.Types.ObjectId, ref: 'Note'}
 });
 
 export const User = mongoose.model<IUser>('User', userSchema);
