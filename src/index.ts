d70efd2c3f99c5e8cb9a67433915e70f03ecd93b
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-06 23:26:58 +0530

    [Vineet]  Refactor :: Add swagger to codebase

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/database';
 import routes from './routes';
+import { setupSwagger } from "./swagger/swagger";
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDocument } from './swagger/swaggerDocument';
 
 dotenv.config();
 
 const app = express();
+
+setupSwagger(app);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use('/api', routes());
