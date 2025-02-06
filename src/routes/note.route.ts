1ee678a71b1758f252143bd501e6f2b2910e8792
VineetChahal <vineet2494.be21@chitkara.edu.in>
2025-02-06 23:25:55 +0530

    [Vineet]  Refactor :: Add getAll route to codebase

--- src/routes/note.route.ts
+++ src/routes/note.route.ts
@@ -13,6 +13,7 @@ class NoteRoutes {
 
     private routes = () => {
         this.router.post('/', createNoteValidation, authenticate, this.NoteController.create);
+        this.router.get('/', authenticate, this.NoteController.getAll);
         this.router.get('/:id', authenticate, this.NoteController.getById);
         this.router.put('/:id', updateNoteValidation, authenticate, this.NoteController.updateById);
         this.router.delete('/:id', authenticate, this.NoteController.deleteById);
